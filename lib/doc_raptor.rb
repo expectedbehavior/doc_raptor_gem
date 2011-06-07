cc336af4bc1a71a99742daaf8725d49b137dca76
Christopher Moore <moorecp@gmail.com>
2011-06-07 13:30:03 -0400

    Added support for new async API

--- lib/doc_raptor.rb
+++ lib/doc_raptor.rb
@@ -20,11 +20,18 @@ class DocRaptor
       :name             => "default",
       :document_type    => "pdf",
       :test             => false,
+      :async            => false,
     }
     options = default_options.merge(options)
+
+    query = { }
+    if options[:async]
+      query[:output => 'json']
+    end
     
-    response = post("/docs", :body => {:doc => options}, :basic_auth => { :username => api_key })
     
+    response = post("/docs", :body => {:doc => options}, :basic_auth => { :username => api_key }, :query => query)
+
     if block_given?
       ret_val = nil
       Tempfile.open("docraptor") do |f|
@@ -35,6 +42,8 @@ class DocRaptor
         ret_val = yield f, response
       end
       ret_val
+    elsif options[:async]
+      self.status_id response.parsed_response["status_id"]
     else
       response
     end
@@ -50,6 +59,43 @@ class DocRaptor
     get("/docs", :query => options, :basic_auth => { :username => api_key })
   end
   
+  def self.status(status_id = @status_id)
+    response = get("/status/#{status_id}", :basic_auth => { :username => api_key }, :output => 'json')
+
+    json = response.parsed_response
+    if json['status'] == 'completed'
+      self.download_key json['download_url'].match(/.*?\/download\/(.+)/)[1]
+    end
+    json
+  end
+
+  def self.download(download_key = @download_key)
+    response = get("/download/#{download_key}")
+    if block_given?
+      ret_val = nil
+      Tempfile.open("docraptor") do |f|
+        f.sync = true
+        f.write(response.body)
+        f.rewind
+
+        ret_val = yield f, response
+      end
+      ret_val
+    else
+      response
+    end
+  end
+
+  def self.status_id(id = nil)
+    return @status_id unless id
+    @status_id = id
+  end
+
+  def self.download_key(key = nil)
+    return @download_key unless key
+    @download_key = key
+  end
+
   base_uri ENV["DOCRAPTOR_URL"] || "https://docraptor.com/"
   api_key  ENV["DOCRAPTOR_API_KEY"]
   
