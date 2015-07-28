b9782b32f6b3502df30ecec71630013b7642c592
Joel Meador <joel@expectedbehavior.com>
2015-07-28 14:57:52 -0400

    some more syntax upgrades to tests

--- test/list_docs_bang_test.rb
+++ test/list_docs_bang_test.rb
@@ -8,20 +8,20 @@ class ListDocsBangTest < MiniTest::Test
 
     describe "with bogus arguments" do
       it "should raise an error if something other than an options hash is passed in" do
-        assert_raises(DocRaptorError::OptionsHashNotHash) {DocRaptor.list_docs!(true)}
-        assert_raises(DocRaptorError::OptionsHashNotHash) {DocRaptor.list_docs!(nil)}
+        assert_raises(DocRaptorError::OptionsHashNotHash) { DocRaptor.list_docs!(true) }
+        assert_raises(DocRaptorError::OptionsHashNotHash) { DocRaptor.list_docs!(nil) }
       end
     end
 
     describe "with good arguments" do
       it "should give me a valid response" do
         stub_http_response_with("simple_list_docs.xml", :get)
-        assert_equal file_fixture("simple_list_docs.xml"), DocRaptor.list_docs!.body
+        assert_equal(file_fixture("simple_list_docs.xml"), DocRaptor.list_docs!.body)
       end
 
       it "raise an exception when the list get fails" do
         stub_http_response_with("invalid_list_docs.xml", :get, 422)
-        assert_raises(DocRaptorException::DocumentListingFailure) {DocRaptor.list_docs!}
+        assert_raises(DocRaptorException::DocumentListingFailure) { DocRaptor.list_docs! }
       end
     end
   end
--- test/list_docs_test.rb
+++ test/list_docs_test.rb
@@ -8,15 +8,15 @@ class ListDocsTest < MiniTest::Test
 
     describe "with bogus arguments" do
       it "should raise an error if something other than an options hash is passed in" do
-        assert_raises(DocRaptorError::OptionsHashNotHash) {DocRaptor.list_docs(true)}
-        assert_raises(DocRaptorError::OptionsHashNotHash) {DocRaptor.list_docs(nil)}
+        assert_raises(DocRaptorError::OptionsHashNotHash) { DocRaptor.list_docs(true) }
+        assert_raises(DocRaptorError::OptionsHashNotHash) { DocRaptor.list_docs(nil) }
       end
     end
 
     describe "with good arguments" do
       it "should give me a valid response" do
         stub_http_response_with("simple_list_docs.xml", :get)
-        assert_equal file_fixture("simple_list_docs.xml"), DocRaptor.list_docs.body
+        assert_equal(file_fixture("simple_list_docs.xml"), DocRaptor.list_docs.body)
       end
     end
   end
